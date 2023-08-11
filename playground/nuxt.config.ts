309ee36ab228cdcd201ed574c01a36cb0b543787
Conner Bachmann <bac@christmann.info>
2023-08-11 22:04:30 +0200

    fix: types & typo

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   modules: ["../src/module"],
   redirects: {
-    verbese: true,
+    verbose: true,
     rules: [
       {
         origin: "/about",
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc["length"] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc["length"]]>;
+type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
+
+export type HTTPStatusCode = IntRange<100, 600>;
